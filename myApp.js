9ec195e4d99a17b894ea8326f601d6c6c7e3f2c0
taufan <taufan@vecapital.asia>
2023-08-16 16:57:38 +0700

    add index html to / route

--- myApp.js
+++ myApp.js
@@ -1,11 +1,9 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World")
-
 
 app.get("/", (req, res) => {
-    res.send("Hello Express");
+    res.sendFile(__dirname + "/public/index.html");
 });
 
 
