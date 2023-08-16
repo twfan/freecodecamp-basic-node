6cc37f14c79e99a05844355f5e8d9f5087e66025
taufan <taufan@vecapital.asia>
2023-08-16 17:18:10 +0700

    add css

--- myApp.js
+++ myApp.js
@@ -2,7 +2,9 @@ let express = require('express');
 let app = express();
 
 
-app.get("/", (req, res) => {
+app.use("/public",express.static(__dirname + "/public"));
+
+app.get("/public", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
 
