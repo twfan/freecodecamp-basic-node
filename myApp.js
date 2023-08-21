33c68b97e51414eb603a7b3a9b2d97da0dd0f9c2
taufan <taufan@vecapital.asia>
2023-08-21 14:32:17 +0700

    test

--- myApp.js
+++ myApp.js
@@ -3,19 +3,18 @@ require('dotenv').config();
 let express = require('express');
 let app = express();
 
-const mySecret = process.env['MESSAGE_STYLE']
-
-
 app.use("/public",express.static(__dirname + "/public"));
 
 app.get("/public", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
 
+process.env.MESSAGE_STYLE='uppercase';
+
 app.get("/json", (req, res) => {
     let response = 'hello json';
 
-    if (mySecret === 'uppercase') {
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
         response = response.toUpperCase();
     }
 
