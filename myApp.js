bfdc31e4b0c7da9ad2c6aa17cc7c16d6a06c5362
taufan <taufan@vecapital.asia>
2023-08-21 14:41:48 +0700

    push

--- myApp.js
+++ myApp.js
@@ -3,24 +3,26 @@ require('dotenv').config();
 let express = require('express');
 let app = express();
 
+
+
+
 app.use("/public",express.static(__dirname + "/public"));
 
 app.get("/public", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
 
-process.env.MESSAGE_STYLE='uppercase';
-
 app.get("/json", (req, res) => {
-    let response = 'hello json';
-
+    let response = 'Hello World'
     if (process.env.MESSAGE_STYLE === 'uppercase') {
-        response = response.toUpperCase();
+        res.json({
+            message: response.toUpperCase()
+        });
+    } else {
+        res.json({
+            message: response
+        });
     }
-
-    res.json({
-        message: response
-    });
 });
 
 
